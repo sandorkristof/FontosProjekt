cf257796872739f6405c797b4f2a819a97c085de
sandorkristof <sandork@kkszki.hu>
2023-09-26 12:10:15 +0200

    Első próba sablonokkal

---
+++ script.js
@@ -0,0 +1,50 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// badge navbarnal  ha van olvasatlan üzenet akkor piros kört mutat 
+
+var unreadMessagesCount = 0; // Az alapértelmezett érték 0
+
+if (unreadMessagesCount > 0) {
+  document.getElementById("unread-badge").style.display = "block";
+} else {
+  document.getElementById("unread-badge").style.display = "none";
+}
+
+
